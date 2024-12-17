6ac115566f946f4a7e76cfef502b4eb37c1ff9ec
smokingplaya <smokingplaya@yandex.ru>
2024-12-18 00:46:44 +0300

    1.0.1

--- lua/autorun/react-gmod.lua
+++ lua/autorun/react-gmod.lua
@@ -8,21 +8,34 @@
 
 --- Config
 -- ? Should we check version of react-gmod or not
-local versionValidatorEnabled = false
+local versionValidatorEnabled = true
 
 --- Constants
 local VERSION = "1.0.0"
 local ADDON_JSON = "https://raw.githubusercontent.com/smokingplaya/react-gmod/refs/heads/main/addon.json"
 
+--- Logs
+local logColor = Color(0, 255, 255)
+local whitespace = " "
+
+local log = function(...)
+  MsgC(logColor, "[react-gmod]", whitespace, color_white, ...)
+  MsgN()
+end
+
 --- addon.json getter
 local getAddonData = function(onSuccess, onFailure)
   http.Fetch(
     ADDON_JSON,
-    function(body)
+    function(body, _, _, code)
+      if (code != 200) then
+        return onFailure("HTTP Code " .. code)
+      end
+
       onSuccess(util.JSONToTable(body))
     end,
     function(message)
-      print("Unable to load react-gmod's data due to:", message)
+      log("Unable to load react-gmod's data due to:", message)
       onFailure(message)
     end
   )
@@ -42,19 +55,20 @@ local validateVersion = function(onSuccess)
 
   getVersion(function(version)
     if (isOutdated(version)) then
-      return print(("You're on an outdated version! Your %s, new %s"):format(VERSION, version))
+      return log(("You're on an outdated version! Your %s, new %s"):format(VERSION, version))
     end
 
     onSuccess()
-  end, function()
-    print("Version check failed, react-gmod will not be enabled.")
+  end, function(error)
+    log("Version check error: " .. error)
+    log("Version check failed, react-gmod will not be enabled.")
   end)
 end
 
 --- Addon load
 if (SERVER) then
   validateVersion(function()
-    print(("React GMod %s by smokingplaya<3"):format(VERSION))
-    print("\thttps://github.com/smokingplaya/react-gmod")
+    log(("React GMod %s by smokingplaya<3"):format(VERSION))
+    log("\thttps://github.com/smokingplaya/react-gmod")
   end)
 end
